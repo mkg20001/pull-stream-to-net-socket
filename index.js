cf89585531ebef004f10cf08f1252a1ee5266b5f
mkg20001 <mkg20001@gmail.com>
2017-10-26 16:26:51 +0200

    Add debug logs

--- index.js
+++ index.js
@@ -3,6 +3,9 @@
 const toStream = require("pull-stream-to-stream")
 const net = require("net")
 
+const debug = require("debug")
+const _log = debug("pull-stream-to-net-socket")
+
 const createServer = () => net.createServer()
 const createClient = (addr) => net.createConnection(addr)
 const EE = require("events").EventEmitter
@@ -10,20 +13,25 @@ const EE = require("events").EventEmitter
 module.exports = function pullStreamToNetSocket(stream, opt, cb) {
   if (!stream || typeof stream == "function" || !stream.sink || !stream.source)
     throw new TypeError("Argument must be a pull-stream duplex. If you only got a source and a sink pass them as { source, sink }")
+  const log = _log.bind(_log, "socket#" + Math.random().toString().replace(/0/g, "").replace(/\./g, "").substr(0, 5))
   if (typeof opt == "function") {
     cb = opt
     opt = {}
   }
   if (!cb) cb = err => err ? console.error(err) : null
   if (!opt) opt = {}
+  log("create", opt)
+  log("create server")
   const server = (opt.createServer || createServer)()
   const ee = new EE()
   let c = {}
   ee.on("conn", (from, conn) => {
+    log("got", from)
     c[from] = conn
     if (c.server && c.client) {
       const client = c.client
       c.client.run = () => {
+        log("establishing circuit")
         const conn = toStream(stream)
         const connserver = c.server
         if (opt.inverse) {
@@ -33,7 +41,9 @@ module.exports = function pullStreamToNetSocket(stream, opt, cb) {
           conn.pipe(connserver) //conn -> (pull) -> server -> (net) -> client
           connserver.pipe(conn) //client -> (net) -> server -> (pull) -> conn
         }
+        log("done")
       }
+      log("calling cb")
       return cb(null, client, c.server)
     }
   })
@@ -41,12 +51,16 @@ module.exports = function pullStreamToNetSocket(stream, opt, cb) {
     if (err) return cb(err)
     const addr = server.address()
     const client = (opt.createClient || createClient)(addr)
+    log("create client")
     client.once("connect", () => {
+      log("client connected")
       ee.emit("conn", "client", client)
     })
     client.once("error", e => cb(e))
     server.once("connection", conn => {
+      log("server-side client connected")
       ee.emit("conn", "server", conn)
+      log("shutting down server")
       server.close()
     })
   })
